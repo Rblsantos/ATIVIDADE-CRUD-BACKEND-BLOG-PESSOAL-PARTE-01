086958da569994c00f2701706ec30dabf0090c71
Ruan Santos <ruan.bsantos@mercadolivre.com>
2022-11-23 14:21:42 -0300

    Adicionando @Repository

--- src/main/java/com/example/atividade_crud_backend_blog_pessoal/Repository/PostagemRepository.java
+++ src/main/java/com/example/atividade_crud_backend_blog_pessoal/Repository/PostagemRepository.java
@@ -2,6 +2,8 @@ package com.example.atividade_crud_backend_blog_pessoal.Repository;
 
 import com.example.atividade_crud_backend_blog_pessoal.Model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 }
