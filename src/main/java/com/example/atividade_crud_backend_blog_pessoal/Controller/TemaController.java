b1766a3f46f94278867ba8bc72c086e268cd7615
Ruan Santos <ruan.bsantos@mercadolivre.com>
2022-11-25 12:34:55 -0300

    Adicionando CrossOrigin

--- src/main/java/com/example/atividade_crud_backend_blog_pessoal/Controller/TemaController.java
+++ src/main/java/com/example/atividade_crud_backend_blog_pessoal/Controller/TemaController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/temas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TemaController {
     @Autowired
     private TemasRepository temasRepository;
